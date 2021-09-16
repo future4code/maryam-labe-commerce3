a70d59883c3d1a8eee092c50868e310fad60d89e
Lucas Gomes <lucasvivomuito@gmail.com>
2021-09-16 15:33:35 -0300

    Em andamento (2)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Product from './Components/Product/Product';
+// import Product from './Components/Product/Product';
+import { ListContainer } from './style';
 
 function App() {
   return (
     <div>
-      <Product></Product>
+      <ListContainer>
+        {this.state.product
+        .filter(product => {
+          return product.filter.toLowerCase().includes(this.state.query.toLowerCase())
+          // description.filter.toLowerCase().includes(this.state.query.toLowerCase())
+        })
+        .filter(product => {
+          return this.state.minPrice === "" || product.Price >== this.state.minPrice
+        })
+        .filter(product => {
+          return this.state.maxPrice === "" || product.Price <== this.state.maxPrice
+        })
+        // .sort((a,b) => {
+        //   switch (this.state.sortingparameter) {
+        //     case "Crescente":
+        //       return a.price - b.price
+        //     default:
+        //       return b.price - a.price
+        //   }
+        // })
+        .map(product =>{
+          return <card key={product.id} product={product} />
+        })}
+      </ListContainer>
     </div>
   );
 }
---
+++ src/style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+display: flex;
+justify-content: space-between;
+flex-wrap: wrap;
+`
+
+// export const FiltersContainer = styled.div``
