82e0dc7d01d73c6febdb97cf6999f53cf0d3d070
Murilo Terenciani <murilo.terenciani@gmail.com>
2021-09-16 18:59:11 -0300

    Layout do site-estatico arrumado

--- src/App.js
+++ src/App.js
@@ -58,35 +58,7 @@ class App extends React.Component {
     </CotainerSite>
     )
   }
-  
-    
-
 }
-
-        {/* <ListContainer>
-          {this.state.products
-          .filter(product => {
-            return product.filter.toLowerCase().includes(this.state.query.toLowerCase())
-            // description.filter.toLowerCase().includes(this.state.query.toLowerCase())
-          })
-          .filter(product => {
-            return this.state.minPrice === "" || product.Price >== this.state.minPrice
-          })
-          .filter(product => {
-            return this.state.maxPrice === "" || product.Price <== this.state.maxPrice
-          })
-          // .sort((a,b) => {
-          //   switch (this.state.sortingparameter) {
-          //     case "Crescente":
-          //       return a.price - b.price
-          //     default:
-          //       return b.price - a.price
-          //   }
-          // })
-          .map(product =>{
-            return <card key={product.id} product={product} />
-          })}
-        </ListContainer> */}
 
 
 export default App;
